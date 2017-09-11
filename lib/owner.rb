7ecf324b0ab89fe961fdc83e61dfa5f72f8ecf2f
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 22:30:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,6 @@ class Owner
 
   def self.count
     OWNERS.size
-
   end
 
   def self.all
@@ -60,12 +59,4 @@ class Owner
   def self.reset_all
     OWNERS.clear
   end
-
-
 end
-
-
-# An owner will know about all its pets, be able to buy a pet,
-# set the name of a pet (which the pet can't change, because that would be weird),
-# change a pet's mood through walking, feeding, or playing with it,
-# and sell all of its pets (for when it moves to that tiny NYC studio after college).
