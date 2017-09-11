1746c875e1d4795d44ec2445b551d0bb290f78cd
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 21:57:30 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,14 @@
 class Owner
-  # code goes here
+
+  attr_accessor :all
+
+  def initialize(name)
+  end
+
 end
+
+
+# An owner will know about all its pets, be able to buy a pet,
+# set the name of a pet (which the pet can't change, because that would be weird),
+# change a pet's mood through walking, feeding, or playing with it,
+# and sell all of its pets (for when it moves to that tiny NYC studio after college).
